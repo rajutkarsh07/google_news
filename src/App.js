f0fa81e0df01b1912ef782b4b655eae9ff3af1ec
utkarsh_raj <rajutkarsh2505@gmail.com>
2022-10-01 23:51:18 +0530

    change in css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import Article from "./component/Article";
+import Switch from "react-switch";
 
 function App() {
   const [news, setNews] = useState([]);
 
   const title = async (title) => {
     const response = await fetch(
-      `https://newsapi.org/v2/everything?q=tesla&from=2022-08-30&sortBy=publishedAt&apiKey=e0e98302734542a992e8ce7a3aa6fdd2`
+      // `https://newsapi.org/v2/everything?q=apple&from=2022-09-30&to=2022-09-30&sortBy=popularity&apiKey=e0e98302734542a992e8ce7a3aa6fdd2`
+      `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e0e98302734542a992e8ce7a3aa6fdd2`
     );
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
 
-    console.log("data articles is  ");
-    console.log(data.articles);
+    // console.log("data articles is  ");
+    // console.log(data.articles);
     setNews(data.articles);
+  };
+
+  const toggleTheme = () => {
+    console.log("hello");
   };
 
   // console.log("news");
@@ -26,6 +32,11 @@ function App() {
 
   return (
     <div className="App">
+      <div className="navbar">
+        <h1>NEWS</h1>
+        <Switch onChange={toggleTheme} />
+      </div>
+
       <div className="news">
         <div className="articles">
           {news.map((news) => {
--- src/component/Article.jsx
+++ src/component/Article.jsx
@@ -3,15 +3,17 @@ import "./Article.css";
 
 const Article = ({ news }) => {
   return (
-    <div className="article">
-      <h2 className="title">{news.title}</h2>
-      <img src={news.urlToImage} alt="images" className="article-image" />
-      <a href={news.url} target="blank" className="article-link">
-        click here to see full article
-      </a>
-      <h3 className="author">{news.author}</h3>
-      <p className="published-date">{news.publishedAt}</p>
-      <p className="content">{news.content}</p>
+    <div className="article" key={news.source.id}>
+      <div className="card">
+        <h2 className="title">{news.title}</h2>
+        <img src={news.urlToImage} alt="images" className="article-image" />
+        <h3 className="author">{news.author}</h3>
+        <p className="published-date">{news.publishedAt}</p>
+        <p className="content">{news.content}</p>
+        <a href={news.url} target="blank" className="article-link">
+          click here to see full article
+        </a>
+      </div>
     </div>
   );
 };
