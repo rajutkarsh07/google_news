3ce4b575d528a457174384a1170fcd9c80da6008
utkarsh_raj <rajutkarsh2505@gmail.com>
2022-10-02 22:53:37 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -11,19 +11,13 @@ function App() {
 
   const title = async (title) => {
     const response = await fetch(
-      // `https://newsapi.org/v2/everything?q=apple&from=2022-09-30&to=2022-09-30&sortBy=popularity&apiKey=e0e98302734542a992e8ce7a3aa6fdd2`
-      // `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e0e98302734542a992e8ce7a3aa6fdd2`
       `https://newsapi.org/v2/everything?q=apple&from=2022-09-30&to=2022-09-30&sortBy=popularity&apiKey=e0e98302734542a992e8ce7a3aa6fdd2`
     );
     const data = await response.json();
-    // console.log(data);
-    // console.log("data articles is  ");
-    // console.log(data.articles);
     setNews(data.articles);
   };
 
   const toggleTheme = () => {
-    console.log("hello");
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
   };
@@ -36,7 +30,14 @@ function App() {
     <div className="App" data-theme={theme}>
       <div className="contents">
         <div className="navbar">
-          <h1 className="nav-header">NEWS</h1>
+          <div className="nav-logo">
+            <img
+              src="https://ssl.gstatic.com/gnews/logo/google_news_192.png"
+              alt="logo"
+              className="g-logo"
+            />
+            <h1 className="nav-header">GOOGLE NEWS</h1>
+          </div>
           {/* <ReactSwitch onChange={toggleTheme} /> */}
           <Switch
             onChange={toggleTheme}
